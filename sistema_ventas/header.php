eb4d15c3532bb31bab651497b276a99df2d3da81
gastonperezmijoff <gmijoff@gmail.com>
2022-09-16 21:14:13 -0300

    cambios

--- sistema_ventas/header.php
+++ sistema_ventas/header.php
@@ -55,7 +55,7 @@ if($_POST){
   <link href="css/bootstrap-select.min.css" rel="stylesheet" type="text/css">
   <script type="text/javascript" src="js/bootstrap-select.min.js"></script>
   <link href="css/estilos.css" rel="stylesheet" type="text/css">
-  <script src="https://cdn.ckeditor.com/ckeditor5/18.0.0/classic/ckeditor.js"></script>
+  <script src="https://cdn.ckeditor.com/ckeditor5/35.0.1/classic/ckeditor.js"></script>
 
 </head>
 
--- sistema_ventas/producto_formularios.php
+++ sistema_ventas/producto_formularios.php
@@ -1,63 +1,72 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Producto formulario</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+include_once("config.php");
+include_once("header.php");
 
-</head>
+?>
 
-<body>
-    <main class="container">
-        <div class="row">
-            <div class="col-12 mb-3">
-                <h1>Productos</h1>
-            </div>
-            <form action="multipart/form-data" method="POST">
-                <div class="row">
-                    <div class="col-12">
-                        <a href="cliente-listado.php" class="btn btn-primary mr-2">Listado</a>
-                        <a href="cliente-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
-                        <button type="submit" class="btn btn-success mr-2" id="btnGuardar" name="btnGuardar">Guardar</button>
-                        <button type="submit" class="btn btn-danger" id="btnBorrar" name="btnBorrar">Borrar</button>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <label for="txtNombre">Nombre</label>
-                        <input type="select" require class="form-control" name="txtNombre" id="txtNombre">
 
-                    </div>
-                    <div class="col-6">
-                        <label for="lstTipoproducto">Tipo de producto</label>
-                        <select name="lstTipoproducto" id="lstTipoproducto" require class="form-control">
-                            <option disabled selected> Seleccionar</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-6">
-                        <label for="txtCantidad">Cantidad</label>
-                        <input type="select" require class="form-control" name="txtcantidad" id="txtcantidad">
-                    </div>
-                    <div class="col-6">
-                        <label for="txtPrecio">Precio</label>
-                        <input type="select" require class="form-control" name="txtPrecio" id="txtPrecio">
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <label for="txtDescripcion">Descripcion</label>
-                        </div>
-                    </div>
-            </form>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 mb-3">
+            <h1>Productos</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 form-group">
+            <a href="cliente-listado.php" class="btn btn-primary mr-2">Listado</a>
+            <a href="cliente-formulario.php" class="btn btn-primary mr-2">Nuevo</a>
+            <button type="submit" class="btn btn-success mr-2" id="btnGuardar" name="btnGuardar">Guardar</button>
+            <button type="submit" class="btn btn-danger" id="btnBorrar" name="btnBorrar">Borrar</button>
         </div>
 
-    </main>
+
+        <div class="col-6 form-group">
+            <label for="txtNombre">Nombre</label>
+            <input type="select" require class="form-control" name="txtNombre" id="txtNombre">
+
+        </div>
+        <div class="col-6 form-group">
+            <label for="lstTipoproducto">Tipo de producto</label>
+            <select name="lstTipoproducto" id="lstTipoproducto" require class="form-control">
+                <option disabled selected> Seleccionar</option>
+            </select>
+        </div>
+
+        <div class="col-6 form-group">
+            <label for="txtCantidad">Cantidad</label>
+            <input type="select" require class="form-control" name="txtcantidad" id="txtcantidad">
+        </div>
+        <div class="col-6 form-group">
+            <label for="txtPrecio">Precio</label>
+            <input type="select" require class="form-control" name="txtPrecio" id="txtPrecio">
+        </div>
+
+        <div class="col-12 form-group">
+            <label for="txtCorreo">Descripcion</label>
+            <textarea type="text" name="txtDescripcion" id="txtdescripcion" class="form-control"></textarea>
+            <script>
+        ClassicEditor
+            .create(document.querySelector('#txtDescripcion'))
+            .catch(error => {
+                console.error(error);
+            });
+    </script>
+        </div>
+    </div>
+
+
+
+
+
+</div>
+
+
 
 
 </body>
+
+
+
 
 </html>
